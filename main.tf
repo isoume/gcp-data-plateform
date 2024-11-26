cbeccb6e23ecfb944c4ccd50e36219e0f2d6663e
Ibrahima SOUMARE <isoumare-ext@galec.fr>
2024-11-26 22:37:07 +0100

    remove the database

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "google_compute_subnetwork" "public_subnet" {
   private_ip_google_access = true
 }
 
+# Allow the private intance to get package in the internet (router)
 resource "google_compute_router" "my_router" {
   name    = "my-router"
   region  = google_compute_subnetwork.private_subnet.region
@@ -29,37 +30,31 @@ resource "google_compute_router" "my_router" {
   }
 }
 
-
-# Step 2: Create a Cloud NAT
+# Allow the private intance to get package in the internet (Network Translation Address)
 resource "google_compute_router_nat" "my_nat" {
-  name   = "my-cloud-nat"
-  router = google_compute_router.my_router.name
-  region = var.private_subnet_region # Same region as your Cloud Router
-
-  nat_ip_allocate_option = "AUTO_ONLY" # Automatically allocated external IPs for NAT
-  # Enable NAT for the specified subnet(s)
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES" # Target specific subnets for NAT
-
-  # Define the subnetwork(s) to use for NAT
-
+  name                               = "my-cloud-nat"
+  router                             = google_compute_router.my_router.name
+  region                             = var.private_subnet_region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-# firewall.tf
+# Allow SSH connection for the vm to test connectivity
 resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh"
   network = google_compute_network.vpc_doctolib.id
 
   allow {
     protocol = "tcp"
-    ports    = ["22"] # Open SSH port
+    ports    = ["22"]
   }
   allow {
     protocol = "icmp"
   }
-  source_ranges = ["0.0.0.0/0"] # Allow from anywhere (customize for security)
+  source_ranges = ["0.0.0.0/0"]
 }
 
-# Firewall Rule: Allow All Egress Traffic
+# Open port for looking upgrade, ...
 resource "google_compute_firewall" "allow_all_egress" {
   name    = "allow-all-egress"
   network = google_compute_network.vpc_doctolib.id
@@ -73,13 +68,13 @@ resource "google_compute_firewall" "allow_all_egress" {
   destination_ranges = ["0.0.0.0/0"]
 }
 
-
-
+# Service account for the vm named (continuous_delevery_vm_sa)
 resource "google_service_account" "continuous_delevery_vm_sa" {
   account_id   = var.vm_continoues_delevery_sa_name
   display_name = var.vm_continoues_delevery_sa_description
 }
 
+# Allow it necessary roles
 resource "google_project_iam_member" "continuous_delevery_vm_sa_role" {
   for_each = var.vm_continues_delevery_sa_roles
 
@@ -88,18 +83,20 @@ resource "google_project_iam_member" "continuous_delevery_vm_sa_role" {
   member  = "serviceAccount:${google_service_account.continuous_delevery_vm_sa.email}"
 }
 
+# Service account for the vm named (data_processing_vm_sa)
 resource "google_service_account" "data_processing_vm_sa" {
   account_id   = var.vm_dataprocessing_sa_name
   display_name = var.vm_dataprocessing_sa_description
 }
 
+# Allow the SA continuous_delevery_vm_sa serviceAccountUser to data_processing_vm_sa for connect
 resource "google_service_account_iam_member" "allow_impersonation_cd_data_processing" {
   service_account_id = "projects/doctolib-data-dev/serviceAccounts/${google_service_account.data_processing_vm_sa.email}"
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.continuous_delevery_vm_sa.email}"
 }
 
-
+# Storage bucket for input data
 module "storage_data_doctolib" {
   source                = "./modules/bucket"
   name                  = "${var.project}-${var.bucket_storage_name}"
@@ -109,6 +106,7 @@ module "storage_data_doctolib" {
   storage_class         = "STANDARD"
 }
 
+# Storage bucket for ARCHIVE
 module "backup_data_doctolib" {
   source                = "./modules/bucket"
   name                  = "${var.project}-${var.bucket_backup_name}"
@@ -118,7 +116,7 @@ module "backup_data_doctolib" {
   storage_class         = "ARCHIVE"
 }
 
-
+# VM that running in the private subnet
 resource "google_compute_instance" "data_processing_vm" {
   name         = "${var.vm_dataprocessing_name}-${var.env}"
   machine_type = "e2-micro"
@@ -137,12 +135,13 @@ resource "google_compute_instance" "data_processing_vm" {
   service_account {
     email = google_service_account.data_processing_vm_sa.email
     scopes = [
-      "https://www.googleapis.com/auth/cloud-platform", # Recommended: broad permissions for general compute tasks
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 
   allow_stopping_for_update = true
 
+  # Install docker for the futur data processing container
   metadata_startup_script = <<-EOT
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl
@@ -160,6 +159,7 @@ resource "google_compute_instance" "data_processing_vm" {
   EOT
 }
 
+# Open the port 8080 for the final users to access on jenkins-allow-http
 resource "google_compute_firewall" "jenkins-allow-http" {
 
   name    = "jenkins-allow-http"
@@ -170,17 +170,14 @@ resource "google_compute_firewall" "jenkins-allow-http" {
     ports    = ["8080"]
   }
 
-  source_ranges = ["0.0.0.0/0"] # Allow traffic from anywhere (can be restricted if needed)
-
-  target_tags = ["jenkins-server"] # Apply the rule only to instances with this tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["jenkins-server"]
 }
 
-
-
+#Provision the vm in the public network
 resource "google_compute_instance" "continuous_delevery_vm" {
   name = "${var.vm_continoues_delevery_name}-${var.env}"
-  #e2-medium
-  #e2-micro
+  # Allow the small to have memory for jenkins and other worker
   machine_type = "e2-small"
   zone         = var.vm_continoues_delevery_zone
   boot_disk {
@@ -192,12 +189,10 @@ resource "google_compute_instance" "continuous_delevery_vm" {
   network_interface {
     network    = google_compute_network.vpc_doctolib.id
     subnetwork = google_compute_subnetwork.public_subnet.id
-
+    # Attach a public network ip address
     access_config {
-      # It automatically assigns an ephemeral public IP
     }
   }
-
 
   service_account {
     email = google_service_account.continuous_delevery_vm_sa.email
@@ -208,6 +203,7 @@ resource "google_compute_instance" "continuous_delevery_vm" {
 
   tags = ["jenkins-server"] # Assign the target tag to the instance
 
+  # Install Jenkins on startup for cd
   metadata_startup_script = <<-EOT
     #!/bin/bash
     sudo apt update
@@ -231,7 +227,7 @@ resource "google_project_service" "service_networking" {
   project = var.project
 }
 
-
+# Let create a private peering/connection for the cloud instance
 resource "google_compute_global_address" "private_google_access" {
   name          = "google-managed-services"
   purpose       = "VPC_PEERING"
@@ -240,6 +236,7 @@ resource "google_compute_global_address" "private_google_access" {
   network       = google_compute_network.vpc_doctolib.id
 }
 
+# Assume the private connection with the peering
 resource "google_service_networking_connection" "private_connection" {
   network = google_compute_network.vpc_doctolib.id
   service = "servicenetworking.googleapis.com"
@@ -248,7 +245,7 @@ resource "google_service_networking_connection" "private_connection" {
   ]
 }
 
-
+# Create the cloud instances
 resource "google_sql_database_instance" "database_doctolib" {
   name             = "dataprocessing-database-${var.env}"
   region           = var.private_subnet_region
@@ -265,7 +262,7 @@ resource "google_sql_database_instance" "database_doctolib" {
   deletion_protection = false
 }
 
-
+# Open traffic in the port of cloud sql m(mysql) 3306
 resource "google_compute_firewall" "allow_private_subnet_traffic" {
   name    = "allow-private-subnet-traffic"
   network = google_compute_network.vpc_doctolib.id
@@ -275,13 +272,8 @@ resource "google_compute_firewall" "allow_private_subnet_traffic" {
     ports    = ["3306"]
   }
 
+  # Only for the private subnet
   source_ranges = [var.private_subnet_range_ip_cidr]
-}
-
-# Create a database within the instance
-resource "google_sql_database" "api_database" {
-  name     = "api_database_v2"
-  instance = google_sql_database_instance.database_doctolib.name
 }
 
 # Grant IAM roles to the VM's Service Account
